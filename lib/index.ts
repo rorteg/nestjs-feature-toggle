1e059829a5996cf5d1406974d1cc8396f6f699bc
Rafael Ortega Bueno <rorteg@gmail.com>
2021-08-30 08:59:50 -0300

    version updated

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export * from './entities';
+export * from './interfaces';
+export * from './feature-toggle-express.middleware';
+export * from './feature-toggle.constants';
+export * from './feature-toggle.module';
+export * from './feature-toggle.providers';
+export * from './feature-toggle.service';
