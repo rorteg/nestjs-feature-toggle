79024185048602b23924218ffe8e3799b8841e30
Rafael Ortega Bueno <rorteg@gmail.com>
2021-09-01 05:37:27 -0300

    refactor: removed unnecessary middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './entities';
 export * from './interfaces';
-export * from './feature-toggle-express.middleware';
 export * from './feature-toggle.constants';
 export * from './feature-toggle.module';
 export * from './feature-toggle.providers';
