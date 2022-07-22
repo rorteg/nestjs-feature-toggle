46624950cc3e29a6883a47354731e7b34af76471
Rafael Ortega Bueno <rorteg@gmail.com>
2022-07-22 12:02:58 -0300

    feat(nestjs-update): maintain compatibility with NestJS version 8
    NestJS 8 contains breaking changes from version 7 and we thought it prudent to release a new major
    release of the package.
    
    BREAKING CHANGE: https://github.com/nestjs/nest/releases/tag/v8.0.0

--- src/feature-toggle.providers.ts
+++ src/feature-toggle.providers.ts
@@ -1,8 +1,9 @@
+import { Provider } from '@nestjs/common';
 import { FeatureToggleModuleOptions } from './interfaces/feature-toggle-module-options.interface';
 import { FEATURE_TOGGLE_MODULE_OPTIONS } from './feature-toggle.constants';
 
 export function createFeatureToggleProvider(
   options: FeatureToggleModuleOptions
-): any[] {
+): Provider[] {
   return [{ provide: FEATURE_TOGGLE_MODULE_OPTIONS, useValue: options || {} }];
 }
